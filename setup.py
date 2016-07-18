53b0e5c99d904c09f8815060354bbef044a777da
david-abel <dave.abel.cs@gmail.com>
2016-07-18 13:12:23 +0100

    Adding setup.py and MANIFEST

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'simple_rl',
+  packages = [],
+  version = '0.1',
+  description = 'A simple framework for experimenting with Reinforcement Learning in Python 2.7',
+  author = 'David Abel',
+  author_email = 'dabel@cs.brown.com',
+  url = 'https://github.com/david-abel/simple_rl',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/0.1',
+  keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
+  classifiers = [],
+)
