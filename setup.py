05332b5282454b17b45917f3e8b76a05ee2b8854
david-abel <dave.abel.cs@gmail.com>
2016-07-25 21:57:27 +0100

    Moving to version 0.21

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'simple_rl',
   packages = [],
-  version = '0.1',
+  version = '0.21',
   description = 'A simple framework for experimenting with Reinforcement Learning in Python 2.7',
   author = 'David Abel',
   author_email = 'dabel@cs.brown.com',
   url = 'https://github.com/david-abel/simple_rl',
-  download_url = 'https://github.com/david-abel/simple_rl/tarball/0.1',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.21',
   keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
   classifiers = [],
 )
