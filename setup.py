9a3f948bef5bbfa3a2b63ea3ac0b59c7e46c94fc
david-abel <dave.abel.cs@gmail.com>
2016-07-27 16:23:15 +0100

    Adding setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'simple_rl',
   packages = [],
-  version = '0.21',
+  version = '0.3',
   description = 'A simple framework for experimenting with Reinforcement Learning in Python 2.7',
   author = 'David Abel',
   author_email = 'dabel@cs.brown.com',
   url = 'https://github.com/david-abel/simple_rl',
-  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.21',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.3',
   keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
   classifiers = [],
 )
