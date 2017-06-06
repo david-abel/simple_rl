fed3674c9db9fb0384f56863d4836b22d78cca98
david-abel <david_abel@brown.edu>
2017-06-06 12:54:47 -0400

    Adding new task init file

--- simple_rl/tasks/__init__.py
+++ simple_rl/tasks/__init__.py
@@ -3,6 +3,7 @@ from chain.ChainMDPClass import ChainMDP
 from chain.ChainStateClass import ChainState
 from grid_world.GridWorldMDPClass import GridWorldMDP
 from grid_world.GridWorldStateClass import GridWorldState
+from four_room.FourRoomMDPClass import FourRoomMDP
 from random.RandomMDPClass import RandomMDP
 from random.RandomStateClass import RandomState
 from taxi.TaxiOOMDPClass import TaxiOOMDP
