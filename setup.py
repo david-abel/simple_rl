377242fe9dbb274afe70c9722d45220c0daaa337
david-abel <david_abel@brown.edu>
2017-06-07 21:23:26 -0400

    fix setup + readme

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
       'sklearn',
       'matplotlib'
   ],
-  version = '0.73',
+  version = '0.732',
   description = 'A simple framework for experimenting with Reinforcement Learning in Python 2.7',
   author = 'David Abel',
   author_email = 'dabel@cs.brown.com',
   url = 'https://github.com/david-abel/simple_rl',
-  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.73',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.732',
   keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
   classifiers = [],
 )
