ea0f4e45642ba6d1b077241608136c19e3676078
david-abel <david_abel@brown.edu>
2017-06-07 21:20:45 -0400

    Adding bandits to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'simple_rl',
   packages = ['simple_rl', 'simple_rl.utils', 'simple_rl.mdp', 'simple_rl.mdp.oomdp',\
-  'simple_rl.agents', 'simple_rl.agents.func_approx', 'simple_rl.experiments', 'simple_rl.tasks',\
+  'simple_rl.agents', 'simple_rl.agents.bandits', 'simple_rl.agents.func_approx', 'simple_rl.experiments', 'simple_rl.tasks',\
   'simple_rl.tasks.chain', 'simple_rl.tasks.random', 'simple_rl.tasks.grid_world', 'simple_rl.tasks.four_room',\
   'simple_rl.tasks.taxi', 'simple_rl.mdp.markov_game', 'simple_rl.tasks.gym',\
   'simple_rl.tasks.grid_game', 'simple_rl.tasks.rock_paper_scissors', 'simple_rl.tasks.prisoners'],
