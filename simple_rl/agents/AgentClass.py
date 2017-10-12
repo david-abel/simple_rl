33efe9231fc3528b360048fe2294dc94ea25ae17
david-abel <david_abel@brown.edu>
2017-10-12 12:49:53 -0400

    Adding get name

--- simple_rl/agents/AgentClass.py
+++ simple_rl/agents/AgentClass.py
@@ -49,5 +49,8 @@ class Agent(object):
     def set_name(self, name):
         self.name = name
 
+    def get_name(self):
+        return self.name
+
     def __str__(self):
         return str(self.name)
