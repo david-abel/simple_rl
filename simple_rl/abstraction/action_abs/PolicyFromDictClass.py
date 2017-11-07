b3374995ab9b35f3149f5d8259b379b893d92549
david-abel <david_abel@brown.edu>
2017-11-07 09:08:19 -0500

    Removing old prints in policy classes

--- simple_rl/abstraction/action_abs/PolicyFromDictClass.py
+++ simple_rl/abstraction/action_abs/PolicyFromDictClass.py
@@ -11,13 +11,9 @@ class PolicyFromDict(Policy):
 		self.policy_dict = policy_dict
 
 	def get_action(self, state):
-		# if state not in self.policy_dict.keys():
-		# 	print "(PolicyFromDict) Abstraction Error:", state, "never seen before."
-		# 	quit()
-
 		if state not in self.policy_dict.keys():
-			# print "(PolicyFromDict) Warning: unseen state (" + str(state) + "). Acting randomly."
+			print "(PolicyFromDict) Warning: unseen state (" + str(state) + "). Acting randomly."
 			return random.choice(list(set(self.policy_dict.values())))
 		else:
-			# print "Seen state!:" + str(state)
+			print "Seen state!:" + str(state)
 			return self.policy_dict[state]
