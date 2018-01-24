ea49c98dd58a916eadbfeb7f9ba8a501cd628a33
David Abel <david_abel@brown.edu>
2018-01-24 12:07:32 -0500

    Fixing default options in abstr wrapper

--- simple_rl/abstraction/AbstractionWrapperClass.py
+++ simple_rl/abstraction/AbstractionWrapperClass.py
@@ -25,7 +25,7 @@ class AbstractionWrapper(Agent):
 
         # Setup the abstracted agent.
         self.agent = SubAgentClass(actions=actions)
-        self.action_abstr = ActionAbstraction(options=self.agent.actions, prim_actions=self.agent.actions) if action_abstr is None else action_abstr
+        self.action_abstr = ActionAbstraction(prim_actions=self.agent.actions) if action_abstr is None else action_abstr
         self.state_abstr = StateAbstraction({}) if state_abstr is None else state_abstr
 
         Agent.__init__(self, name=self.agent.name + "-" + name_ext, actions=self.action_abstr.get_actions())
