8bfd6790d0994380be4041dc49caca486160517b
David Abel <david_abel@brown.edu>
2018-02-25 15:11:14 -0500

    Adding DQN to gym example

--- examples/gym_example.py
+++ examples/gym_example.py
@@ -6,19 +6,19 @@ import logging
 
 # Other imports.
 import srl_example_setup
-from simple_rl.agents import LinearQLearningAgent, RandomAgent
+from simple_rl.agents import RandomAgent, DQNAgent
 from simple_rl.tasks import GymMDP
 from simple_rl.run_experiments import run_agents_on_mdp
 
 def main(open_plot=True):
     # Gym MDP
-    gym_mdp = GymMDP(env_name='Acrobot-v1', render=True)
+    gym_mdp = GymMDP(env_name='Breakout-v0', render=False)
     num_feats = gym_mdp.get_num_state_feats()
 
     # Setup agents and run.
-    lin_agent = LinearQLearningAgent(gym_mdp.get_actions(), num_features=num_feats, rbf=True, alpha=0.1, epsilon=0.1, anneal=True)
     rand_agent = RandomAgent(gym_mdp.get_actions())
-    run_agents_on_mdp([lin_agent, rand_agent], gym_mdp, instances=50, episodes=50, steps=100, open_plot=open_plot)
+    dqn_agent = DQNAgent(gym_mdp.get_actions())
+    run_agents_on_mdp([dqn_agent, rand_agent], gym_mdp, instances=3, episodes=20, steps=200, open_plot=open_plot, verbose=True)
 
 if __name__ == "__main__":
     LOGGER = logging.getLogger(__name__)
