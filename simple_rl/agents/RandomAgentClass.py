2cfda076b9355745f6bb549548d2b3b4b396d010
yrevar <yagneshrevar@gmail.com>
2018-03-28 10:09:14 -0700

    saw errors while running setup.py on python3, fixing few prints and inconsistent indentations

--- simple_rl/agents/RandomAgentClass.py
+++ simple_rl/agents/RandomAgentClass.py
@@ -10,7 +10,7 @@ class RandomAgent(Agent):
     ''' Class for a random decision maker. '''
 
     def __init__(self, actions, name=""):
-    	name = "Random" if name is "" else name
+        name = "Random" if name is "" else name
         Agent.__init__(self, name=name, actions=actions)
 
     def act(self, state, reward):
