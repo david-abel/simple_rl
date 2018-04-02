e6513ae197699a2ac158ac01b0018a116c1a5144
David Abel <david_abel@brown.edu>
2018-04-02 18:23:13 -0400

    Prep for merge

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'simple_rl',
   packages = ['simple_rl', 'simple_rl.utils', 'simple_rl.mdp', 'simple_rl.mdp.oomdp',\
@@ -8,16 +8,12 @@ setup(
   'simple_rl.tasks.grid_game', 'simple_rl.tasks.rock_paper_scissors', 'simple_rl.tasks.prisoners', \
   'simple_rl.abstraction', 'simple_rl.abstraction.action_abs', 'simple_rl.abstraction.state_abs'],
   scripts=['simple_rl/run_experiments.py'],
-  install_requires=[
-      'numpy',
-      'matplotlib'
-  ],
-  version='0.76',
+  version='0.77',
   description = 'A simple framework for experimenting with Reinforcement Learning in Python.',
   author = 'David Abel',
   author_email = 'david_abel@brown.edu',
   url = 'https://github.com/david-abel/simple_rl',
-  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.76',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.77',
   keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
   classifiers = [],
 )
