3e4d92e51c05a2227b0261a1c93631e480b2109c
David Abel <david_abel@brown.edu>
2018-04-13 07:24:30 -0400

    Fixing version

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 from setuptools import find_packages
+exec(open('simple_rl/_version.py').read())
 setup(
   name = 'simple_rl',
   packages = find_packages(),
   scripts=['simple_rl/run_experiments.py'],
-  version='0.77',
+  version=__version__,
   description = 'A simple framework for experimenting with Reinforcement Learning in Python.',
   author = 'David Abel',
   author_email = 'david_abel@brown.edu',
   url = 'https://github.com/david-abel/simple_rl',
-  download_url = 'https://github.com/david-abel/simple_rl/tarball/v0.77',
+  download_url = 'https://github.com/david-abel/simple_rl/tarball/v' + __version__,
   keywords = ['Markov Decision Process', 'MDP', 'Reinforcement Learning'],
   classifiers = [],
 )
--- simple_rl/__init__.py
+++ simple_rl/__init__.py
@@ -5,6 +5,7 @@ simple_rl
 		QLearningAgentClass.py
 		RandomAgentClass.py
 		RMaxAgentClass.py
+		...
 	experiments/
 		ExperimentClass.py
 		ExperimentParameters.py
@@ -18,22 +19,25 @@ simple_rl
 		grid_world/
 			GridWorldMPDClass.py
 			GridWorldStateClass.py
+		...
 	utils/
 		chart_utils.py
+		make_mdp.py
 	run_experiments.py
 
 Author and Maintainer: David Abel (cs.brown.edu/~dabel/)
-Last Updated: July 25th, 2017
+Last Updated: April 13th, 2018
 Contact: dabel@cs.brown.edu
-License: MIT
+License: Apache
 '''
-# Fix xrange.
+# Fix xrange to cooperate with python 2 and 3.
 try:
     xrange
 except NameError:
     xrange = range
 
 # Imports.
-
 import simple_rl.agents, simple_rl.experiments, simple_rl.mdp, simple_rl.planning, simple_rl.tasks, simple_rl.utils
 import simple_rl.run_experiments
+
+from simple_rl._version import __version__
