7da696efca199027f1e102d673fa54d3747ac7ee
David Abel <david_abel@brown.edu>
2018-04-21 10:13:48 -0400

    Adding explicit imports to __init__

--- simple_rl/abstraction/__init__.py
+++ simple_rl/abstraction/__init__.py
@@ -1,7 +1,15 @@
+# Classes.
 from simple_rl.abstraction.AbstractionWrapperClass import AbstractionWrapper
 from simple_rl.abstraction.AbstractValueIterationClass import AbstractValueIteration
 from simple_rl.abstraction.state_abs.StateAbstractionClass import StateAbstraction
 from simple_rl.abstraction.action_abs.ActionAbstractionClass import ActionAbstraction
+from simple_rl.abstraction.action_abs.InListPredicateClass import InListPredicate
+from simple_rl.abstraction.action_abs.OptionClass import Option
+from simple_rl.abstraction.action_abs.PolicyClass import Policy
+from simple_rl.abstraction.action_abs.PolicyFromDictClass import PolicyFromDict
+from simple_rl.abstraction.action_abs.PredicateClass import Predicate
+
+# Scripts.
 from simple_rl.abstraction.state_abs import sa_helpers, indicator_funcs
 from simple_rl.abstraction.action_abs import aa_helpers
 from simple_rl.abstraction.make_abstr_mdp import make_abstr_mdp
