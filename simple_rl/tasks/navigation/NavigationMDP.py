0d64f0eb8da06f1a380e938ec58593f4ab3455b9
yrevar <yagneshrevar@gmail.com>
2018-04-23 21:58:16 -0700

    navigation mdp: improves visualization and adds exp1 features for short and long horizons

--- simple_rl/tasks/navigation/NavigationMDP.py
+++ simple_rl/tasks/navigation/NavigationMDP.py
@@ -22,7 +22,7 @@ class NavigationMDP(GridWorldMDP):
                  rand_init=True,
                  goal_locs=[(21, 21)],
                  cell_types=["empty", "yellow", "red", "green", "purple"],
-                 cell_rewards=[0, 0, -10, -10, -10],
+                 cell_type_rewards=[0, 0, -10, -10, -10],
                  gamma=0.99,
                  slip_prob=0.00,
                  step_cost=0.0,
@@ -52,7 +52,8 @@ class NavigationMDP(GridWorldMDP):
         self.cell_prob = [4.*vacancy_prob/5., vacancy_prob/5.] + [(1-vacancy_prob)/3.] * 3
         # Matrix for identifying cell type and associated reward
         self.cells = np.random.choice(len(self.cell_types), p=self.cell_prob, size=height*width).reshape(height,width)
-        self.cell_rewards = np.asarray([[cell_rewards[item] for item in row] for row in self.cells]).reshape(height,width)
+        self.cell_type_rewards = cell_type_rewards
+        self.cell_rewards = np.asarray([[cell_type_rewards[item] for item in row] for row in self.cells]).reshape(height,width)
         self.goal_reward = goal_reward
 
         GridWorldMDP.__init__(self, 
@@ -72,8 +73,17 @@ class NavigationMDP(GridWorldMDP):
 
         # Set goals and their rewards in the matrix
         for g in goal_locs:
-            self.cells[self.height-(g[1]), g[0]-1] = len(self.cell_types) # allocate the next type to the goal
-            self.cell_rewards[self.height-(g[1]), g[0]-1] = self.goal_reward
+            g_r, g_c = self._xy_to_rowcol(g[0], g[1])
+            self.cells[g_r, g_c] = len(self.cell_types) # allocate the next type to the goal
+            self.cell_rewards[g_r, g_c] = self.goal_reward
+        self.goal_locs = goal_locs
+
+        self.loc_cells = [np.vstack(np.where(self.cells == cell)).transpose() for cell in range(len(self.cell_types))]
+        self.feature_cell_dist = np.zeros(self.cells.shape + (len(self.cell_types),), np.float32)
+        for row in range(self.height):
+            for col in range(self.width):
+                self.feature_cell_dist[row, col] = [np.linalg.norm([row, col] - loc_cell, axis=1).min() \
+                                                                                        for loc_cell in self.loc_cells]
 
     def _reward_func(self, state, action):
         '''
@@ -84,12 +94,25 @@ class NavigationMDP(GridWorldMDP):
         Returns
             (float)
         '''
+        r, c = self._xy_to_rowcol(state.x, state.y)
         if self._is_goal_state_action(state, action):
-            return self.goal_reward + self.cell_rewards[self.height-(state.y), state.x-1] - self.step_cost
-        elif self.cell_rewards[self.height-(state.y), state.x-1] == 0:
+            return self.goal_reward + self.cell_rewards[r, c] - self.step_cost
+        elif self.cell_rewards[r, c] == 0:
             return 0 - self.step_cost
         else:
-            return self.cell_rewards[self.height-(state.y), state.x-1]
+            return self.cell_rewards[r, c]
+
+    def _xy_to_rowcol(self, x, y):
+        """
+        Converts (x,y) to (row,col) 
+        """
+        return self.height - y, x - 1
+
+    def _rowcol_to_xy(self, row, col):
+        """
+        Converts (row,col) to (x,y) 
+        """
+        return col + 1, self.height - row
 
     def get_random_init_state(self):
         """
@@ -97,25 +120,44 @@ class NavigationMDP(GridWorldMDP):
         """
         rows, cols = np.where(self.cells == 0)
         rand_idx = np.random.randint(len(rows))
-        return GridWorldState(cols[rand_idx]+1, self.height-rows[rand_idx])
+        x, y = self._rowcol_to_xy(rows[rand_idx], cols[rand_idx])
+        return GridWorldState(x, y)
 
-    def visualize_grid(self, trajectories=None):
+    def visualize_grid(self, values=None, cmap=None,
+                                trajectories=None, subplot_str=None,
+                                new_fig=True, title="Navigation MDP"):
         """
         Args:
             trajectories ([[state1, state2, ...], [state7, state4, ...], ...]): trajectories to be shown on the grid
         """
         import matplotlib.pyplot as plt
         from matplotlib import colors
-        plt.figure(figsize=( max(self.height//4, 6), max(self.width//4, 6)))
-        cmap = colors.ListedColormap(['white','yellow','red','lime','magenta', 'blue'])
-        plt.imshow(self.cells, cmap=cmap)
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+        cell_types = ['white', 'yellow', 'red', 'lime', 'magenta', 'blue']
+        cell_type_rewards = self.cell_type_rewards + [self.goal_reward]
+
+        if new_fig == True:
+            plt.figure(figsize=(max(self.height // 4, 6), max(self.width // 4, 6)))
+
+        if subplot_str is not None:
+            plt.subplot(subplot_str)
+
+        if cmap is None:
+            cmap = colors.ListedColormap(cell_types)
+
+        if values is None:
+            values = self.cells.copy()
+
+        im = plt.imshow(values, cmap=cmap)
+        plt.title(title)
         ax = plt.gca()
         ax.set_xticklabels('')
         ax.set_yticklabels('')
         ax.set_xticks(np.arange(self.width), minor=True)
         ax.set_yticks(np.arange(self.height), minor=True)
-        ax.set_xticklabels(1+np.arange(self.width), minor=True, fontsize=8)
-        ax.set_yticklabels(1+np.arange(self.height)[::-1], minor=True, fontsize=8)
+        ax.set_xticklabels(1 + np.arange(self.width), minor=True, fontsize=8)
+        ax.set_yticklabels(1 + np.arange(self.height)[::-1], minor=True, fontsize=8)
 
         if trajectories is not None and trajectories:
             for state_seq in trajectories:
@@ -123,4 +165,11 @@ class NavigationMDP(GridWorldMDP):
                 path_ys = [self.height - (s.y) for s in state_seq]
                 plt.plot(path_xs[0], path_ys[0], ".k", markersize=10)
                 plt.plot(path_xs, path_ys, "k", linewidth=0.7)
-        plt.show()
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="3%", pad=0.05)
+        cb = plt.colorbar(im, cax=cax)
+        
+        if subplot_str is None:
+            plt.show()
+
