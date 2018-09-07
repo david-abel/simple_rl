056078c033a2c8157b472a0ab1315cdb39351779
David Abel <david_abel@brown.edu>
2018-09-07 13:38:22 -0700

    Adding step cost to simple_rl

--- simple_rl/tasks/puddle/PuddleMDPClass.py
+++ simple_rl/tasks/puddle/PuddleMDPClass.py
@@ -17,7 +17,7 @@ from simple_rl.tasks.grid_world.GridWorldStateClass import GridWorldState
 class PuddleMDP(GridWorldMDP):
     ''' Class for a Puddle MDP '''
 
-    def __init__(self, gamma=0.99, slip_prob=0.00, name="puddle", puddle_rects=[(0.1, 0.8, 0.5, 0.7), (0.4, 0.7, 0.5, 0.4)], is_goal_terminal=True, rand_init=False):
+    def __init__(self, gamma=0.99, slip_prob=0.00, name="puddle", puddle_rects=[(0.1, 0.8, 0.5, 0.7), (0.4, 0.7, 0.5, 0.4)], is_goal_terminal=True, rand_init=False, step_cost=0.0):
         '''
         Args:
             gamma (float)
@@ -26,10 +26,11 @@ class PuddleMDP(GridWorldMDP):
             puddle_rects (list): [(top_left_x, top_left_y), (bot_right_x, bot_right_y)]
             is_goal_terminal (bool)
             rand_init (bool)
+            step_cost (float)
         '''
         self.delta = 0.05
         self.puddle_rects = puddle_rects
-        GridWorldMDP.__init__(self, width=1.0, height=1.0, init_loc=[0.25, 0.6], goal_locs=[[1.0, 1.0]], gamma=gamma, name=name, is_goal_terminal=is_goal_terminal, rand_init=rand_init)
+        GridWorldMDP.__init__(self, width=1.0, height=1.0, init_loc=[0.25, 0.6], goal_locs=[[1.0, 1.0]], gamma=gamma, name=name, is_goal_terminal=is_goal_terminal, rand_init=rand_init, step_cost=step_cost)
 
     def get_parameters(self):
         '''
