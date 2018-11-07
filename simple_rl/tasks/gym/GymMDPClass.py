2f96825e757439a93ef2b35db3724f512f38a3e8
David Abel <david_abel@brown.edu>
2018-11-07 15:08:32 -0500

    adding render flag to gym mdp

--- simple_rl/tasks/gym/GymMDPClass.py
+++ simple_rl/tasks/gym/GymMDPClass.py
@@ -7,6 +7,7 @@ import random
 import sys
 import os
 import random
+from collections import defaultdict
 
 # Other imports.
 import gym
@@ -16,11 +17,16 @@ from simple_rl.tasks.gym.GymStateClass import GymState
 class GymMDP(MDP):
     ''' Class for Gym MDPs '''
 
-    def __init__(self, env_name='CartPole-v0', render=False):
+    def __init__(self, env_name='CartPole-v0', render=False, render_every_n_episodes=0):
         '''
         Args:
             env_name (str)
+            render (bool): If True, renders the screen every time step.
+            render_every_n_epsiodes (int): @render must be True, then renders the screen every n episodes.
         '''
+        # self.render_every_n_steps = render_every_n_steps
+        self.render_every_n_episodes = render_every_n_episodes
+        self.episode = 0
         self.env_name = env_name
         self.env = gym.make(env_name)
         self.render = render
@@ -47,7 +53,7 @@ class GymMDP(MDP):
         '''
         obs, reward, is_terminal, info = self.env.step(action)
 
-        if self.render:
+        if self.render and self.episode % self.render_every_n_episodes == 0:
             self.env.render()
 
         self.next_state = GymState(obs, is_terminal=is_terminal)
@@ -67,6 +73,7 @@ class GymMDP(MDP):
 
     def reset(self):
         self.env.reset()
+        self.episode += 1
 
     def __str__(self):
         return "gym-" + str(self.env_name)
