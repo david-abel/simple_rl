25754164d7ec17ff695836a1608f0f3725d924ea
NishanthJKumar <nishanth.kumar20@gmail.com>
2019-05-28 16:35:48 -0400

    Created example for Q-learning with potential initialization

--- examples/viz_example.py
+++ examples/viz_example.py
@@ -15,7 +15,7 @@ from simple_rl.planning import ValueIteration
 def parse_args():
     # Add all arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", type=str, default="value", nargs='?', help="Choose the visualization type (one of {value, policy, agent}).")
+    parser.add_argument("-v", type=str, default="learning", nargs='?', help="Choose the visualization type (one of {value, policy, agent, learning or interactive}).")
     args = parser.parse_args()
     return args.v
 
@@ -25,9 +25,6 @@ def main():
     mdp = GridWorldMDP(width=4, height=3, init_loc=(1, 1), goal_locs=[(4, 3)], lava_locs=[(4, 2)], gamma=0.95, walls=[(2, 2)], slip_prob=0.1)
     ql_agent = QLearningAgent(mdp.get_actions(), epsilon=0.2, alpha=0.2) 
     viz = parse_args()
-
-    # Choose viz type.
-    viz = "value"
 
     if viz == "value":
         # --> Color corresponds to higher value.
@@ -47,7 +44,7 @@ def main():
         mdp.visualize_agent(ql_agent)
     elif viz == "learning":
         # --> Press <r> to reset.
-        # Run experiment and make plot.
+        # Show agent's interaction with the environment.
         mdp.visualize_learning(ql_agent, delay=0.005, num_ep=500, num_steps=200)
     elif viz == "interactive":
         # Press <1>, <2>, <3>, and so on to execute action 1, action 2, etc.
