2924e2aa49900fedba4dc878cc2f65064824e4dc
david-abel <david_abel@brown.edu>
2019-10-28 10:59:46 -0400

    Removing tkagg from gather

--- simple_rl/tasks/gather/GatherStateClass.py
+++ simple_rl/tasks/gather/GatherStateClass.py
@@ -5,7 +5,6 @@ from simple_rl.mdp.StateClass import State
 import numpy as np
 import time
 import matplotlib
-matplotlib.use('TkAgg')   
 import matplotlib.pyplot as plt # NOTE: for debugging
 
 COLORS = {
