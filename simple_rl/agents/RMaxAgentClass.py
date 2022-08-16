3df5a7513264b9a293e22aa77a31a74d5c92f072
Zhiyuan "Paul" Zhou <61770864+zhouzypaul@users.noreply.github.com>
2022-08-16 11:41:13 -0400

    fix RMAX and speed it up with numpy

--- simple_rl/agents/RMaxAgentClass.py
+++ simple_rl/agents/RMaxAgentClass.py
@@ -4,65 +4,75 @@ RMaxAgentClass.py: Class for an RMaxAgent from [Strehl, Li and Littman 2009].
 Notes:
     - Assumes WLOG reward function codomain is [0,1] (so RMAX is 1.0)
 '''
-
-# Python imports.
+import math
 import random
-import numpy as np
 from collections import defaultdict
 
-# Local classes.
+import numpy as np
+
 from simple_rl.agents.AgentClass import Agent
+
 
 class RMaxAgent(Agent):
     '''
     Implementation for an R-Max Agent [Strehl, Li and Littman 2009]
+    https://jmlr.org/papers/volume10/strehl09a/strehl09a.pdf
+    pseudocode described in the PDF
     '''
 
-    def __init__(self, actions, gamma=0.95, s_a_threshold=2, epsilon_one=0.99, max_reward=1.0, name="RMax", custom_q_init=None):
+    def __init__(self, 
+                states, 
+                actions, 
+                gamma=0.95, 
+                s_a_threshold=2, 
+                epsilon_one=0.99, 
+                max_reward=1.0, 
+                name="RMax", 
+                custom_q_init=None):
+        """
+        Args:
+            states: a list of states in the MDP. Simply range(n_states) would suffice
+            actions: a list of actions in the MDP. Simply range(n_actions) would suffice
+            gamma: discount factor
+            s_a_threshold: the number of (s, a) transitions, first seen, to build the model of the MDP
+            epsilon_one: param used to decide how many iterations of value iterations to do
+            max_reward: the expected maximum reward of the MDP
+            name: name of the agent
+            custom_q_init: a (n_states, n_actions) numpy array specifying the initial Q values to use
+        """
         self.name = name 
+        self.states = list(states)
         Agent.__init__(self, name=name, actions=actions, gamma=gamma)
         self.rmax = max_reward
         self.s_a_threshold = s_a_threshold
         self.custom_q_init = custom_q_init
-        self.reset()
-        self.custom_q_init = custom_q_init
-        self.gamma = gamma
         self.epsilon_one = epsilon_one
+        self.max_num_value_iter = math.ceil(np.log(1/(self.epsilon_one * (1 - self.gamma))) / (1 - self.gamma))
 
-        if self.custom_q_init:
-            self.q_func = self.custom_q_init
-        else:
-            self.q_func = defaultdict(lambda: defaultdict(lambda: self.rmax))
+        self.reset()
 
     def reset(self):
         '''
         Summary:
-            Resets the agent back to its tabula rasa config.
+            Resets the agent back to its tabular asa config.
         '''
-        self.rewards = defaultdict(lambda : defaultdict(list)) # S --> A --> reward
-        self.transitions = defaultdict(lambda : defaultdict(lambda : defaultdict(int))) # S --> A --> S' --> counts
-        self.r_s_a_counts = defaultdict(lambda : defaultdict(int)) # S --> A --> #rs
-        self.t_s_a_counts = defaultdict(lambda : defaultdict(int)) # S --> A --> #ts
+        self.rewards = np.zeros((len(self.states), len(self.actions)))  # rewards are assumed to be state-action based R(s, a)
+        self.transitions = np.zeros((len(self.states), len(self.actions), len(self.states)))
+        self.s_a_counts = np.zeros((len(self.states), len(self.actions)))
         self.prev_state = None
         self.prev_action = None
 
         if self.custom_q_init:
             self.q_func = self.custom_q_init
         else:
-            self.q_func = defaultdict(lambda: defaultdict(lambda: self.rmax))
-
-    def get_num_known_sa(self):
-        return sum([self.is_known(s,a) for s,a in self.r_s_a_counts.keys()])
-
-    def is_known(self, s, a):
-        return self.r_s_a_counts[s][a] >= self.s_a_threshold and self.t_s_a_counts[s][a] >= self.s_a_threshold
+            self.q_func = np.ones((len(self.states), len(self.actions))) * self.rmax * 1/(1-self.gamma)
 
     def act(self, state, reward):
-        # Update given s, a, r, s' : self.prev_state, self.prev_action, reward, state
-        self.update(self.prev_state, self.prev_action, reward, state)
-
         # Compute best action by argmaxing over Q values of all possible s,a pairs
         action = self.get_max_q_action(state)
+
+        # Update given s, a, r, s' : self.prev_state, self.prev_action, reward, state
+        self.update(self.prev_state, self.prev_action, reward, state)
 
         # Update pointers.
         self.prev_action = action
@@ -82,33 +92,51 @@ class RMaxAgent(Agent):
             Updates T and R.
         '''
         if state != None and action != None:
-            if self.r_s_a_counts[state][action] <= self.s_a_threshold or self.t_s_a_counts[state][action] <= self.s_a_threshold:
+            if self.s_a_counts[state][action] < self.s_a_threshold:
                 # Add new data points if we haven't seen this s-a enough.
-                self.rewards[state][action] += [reward]
-                self.r_s_a_counts[state][action] += 1
+                self.rewards[state][action] += reward
+                self.s_a_counts[state][action] += 1
                 self.transitions[state][action][next_state] += 1
-                self.t_s_a_counts[state][action] += 1
 
-                if self.r_s_a_counts[state][action] == self.s_a_threshold:
-                    # Start updating Q values for subsequent states
-                    lim = int(np.log(1/(self.epsilon_one * (1 - self.gamma))) / (1 - self.gamma))
-                    for i in range(1, lim):
-                        for curr_state in self.rewards.keys():
-                            for curr_action in self.actions:
-                                if self.r_s_a_counts[curr_state][curr_action] >= self.s_a_threshold:
-                                    self.q_func[curr_state][curr_action] = self._get_reward(curr_state, curr_action) + (self.gamma * self.get_transition_q_value(curr_state, curr_action))
+                if self.s_a_counts[state][action] == self.s_a_threshold:
+                    self.value_iteration()
 
-    def get_transition_q_value(self, state, action):
+    def value_iteration(self):
         '''
-        Args: 
-            state
-            action 
-
-        Returns:
-            empirical transition probability 
+        Do some iterations of value iteration to compute the q values
+        Only update the (s, a) pairs that have enough experiences seen
+        Q(s, a) = R(s, a) + gamma * \sum_s' T(s, a, s') * max_a' Q(s', a')
         '''
-        return sum([(self._get_transition(state, action, next_state) * self.get_max_q_value(next_state)) for next_state in self.q_func.keys()])
+        # mask for update
+        mask = self.s_a_counts >= self.s_a_threshold
+        pseudo_count = np.where(self.s_a_counts == 0, 1, self.s_a_counts)  # avoid divide by zero
 
+        # build the reward model
+        empirical_reward_mat = self.rewards / pseudo_count
+
+        # build the transition model: assume self-loop if there's not enough data
+        # assume a self-loop if there's not enough data
+        empirical_transition_mat = self.transitions / pseudo_count[:, :, None]
+        # only masked positions should be trusted, otherwise self transition
+
+        empirical_transition_mat[~mask] = self._self_transition_mat[~mask]
+        assert np.all(empirical_transition_mat.sum(axis=-1) == 1)
+
+        # compute the update for every (s, a), but only apply the ones that needed with a mask
+        for i in range(self.max_num_value_iter):
+            v = np.max(self.q_func, axis=-1)
+            new_q = empirical_reward_mat + self.gamma * np.einsum("san,n->sa", empirical_transition_mat, v)
+            if np.all(np.abs(self.q_func[mask] - new_q[mask]) < 1e-4):
+                break
+            self.q_func[mask] = new_q[mask]
+
+    def _self_transition_mat(self):
+        '''
+        create a transition matrix where each state just transition to itself
+        '''
+        self_transition_mat = np.zeros_like(self.transitions)
+        self_transition_mat[np.arange(len(self.states)), :, np.arange(len(self.states))] = 1
+        return self_transition_mat
 
     def get_value(self, state):
         '''
@@ -129,15 +157,12 @@ class RMaxAgent(Agent):
             (tuple) --> (float, str): where the float is the Qval, str is the action.
         '''
         # Grab random initial action in case all equal
-        best_action = random.choice(self.actions)
-        max_q_val = self.get_q_value(state, best_action)
+        if np.all(self.q_func[state] == self.q_func[state, 0]):
+            best_action = self.rng.choice(self.actions)
+        else:
+            best_action =np.argmax(self.q_func[state])
+        max_q_val = self.q_func[state][best_action]
 
-        # Find best action (action w/ current max predicted Q value) 
-        for action in self.actions:
-            q_s_a = self.get_q_value(state, action)
-            if q_s_a > max_q_val:
-                max_q_val = q_s_a
-                best_action = action
         return max_q_val, best_action
 
     def get_max_q_action(self, state):
@@ -171,35 +196,3 @@ class RMaxAgent(Agent):
         '''
 
         return self.q_func[state][action]
-
-    def _get_reward(self, state, action):
-        '''
-        Args:
-            state (State)
-            action (str)
-
-        Returns:
-            Believed reward of executing @action in @state. If R(s,a) is unknown
-            for this s,a pair, return self.rmax. Otherwise, return the MLE.
-        '''
-
-        if self.r_s_a_counts[state][action] >= self.s_a_threshold:
-            # Compute MLE if we've seen this s,a pair enough.
-            rewards_s_a = self.rewards[state][action]
-            return float(sum(rewards_s_a)) / len(rewards_s_a)
-        else:
-            # Otherwise return rmax.
-            return self.rmax
-    
-    def _get_transition(self, state, action, next_state):
-        '''
-        Args: 
-            state (State)
-            action (str)
-            next_state (str)
-
-            Returns:
-                Empirical probability of transition n(s,a,s')/n(s,a) 
-        '''
-
-        return self.transitions[state][action][next_state] / self.t_s_a_counts[state][action]
